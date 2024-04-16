d00999eba5b1e147f9844e8aac77685343f25db7
Gabriel Duenas <duenasdevelops@gmail.com>
2024-04-15 22:54:26 -0700

    Update: Intialize pipeline and ensure zenml dashboard integration

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -23,7 +23,7 @@ class IngestData:
     
 
 @step 
-def ingest_data(data_path: str) -> pd.DataFrame:
+def ingest_df(data_path: str) -> pd.DataFrame:
     """
     Ingesting the data from the data_path.
 
